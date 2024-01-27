6324c58eb645eff8c7e21a0e9f578b8f61ddcfdd
Arsenii Berezin <b-ars07@yandex.ru>
2024-01-28 01:51:33 +0300

    [ex4] - Type Guard для запроса

--- src/ex_4/ex_4.ts
+++ src/ex_4/ex_4.ts
@@ -23,3 +23,19 @@ interface IResponseFailed {
     status: PaymentStatus.Failed;
     data: IDataFailed;
 }
+
+type Res = IResponseSuccess | IResponseFailed
+
+function isDatabaseId(res: Res): res is IResponseSuccess {
+    return 'databaseId' in res
+}
+
+function isSuccessPayment(res: Res): number {
+    if (isDatabaseId(res)) {
+        return res.data.databaseId
+    } else {
+        throw new Error(res.data.errorMessage)
+    }
+}
+
+
